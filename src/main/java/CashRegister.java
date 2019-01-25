1ab450df1ac944853471b4027cd0997c44fcc1e3
Romy <contact@romyalula.com>
2019-01-25 20:28:19 +0100

    CashRegister : Calcul total price for a given price and a given quantity

---
+++ src/main/java/CashRegister.java
@@ -0,0 +1,6 @@
+public class CashRegister {
+
+    public double total(double price, int quantity) {
+        return price * quantity;
+    }
+}
--- src/test/java/ThingTest.java
+++ src/test/java/ThingTest.java
@@ -4,7 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ThingTest {
     @Test
-    public void name() {
-        assertThat(0).isEqualTo(0);
+    public void with_a_price_and_a_quantity_should_return_the_product_of_both() {
+        CashRegister cashRegister = new CashRegister();
+        double price = 1.20;
+        int quantity = 1;
+        double total = cashRegister.total(price, quantity);
+        assertThat(total).isEqualTo(1.20);
     }
 }
