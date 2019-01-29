c3ef7304ddd6172d621391bc34fa0595fb1628fb
Romy <contact@romyalula.com>
2019-01-29 21:49:42 +0100

    Cash Register : Search for price with generified reduce

--- src/main/java/PriceQuery.java
+++ src/main/java/PriceQuery.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
 class PriceQuery {
     private final ItemReference[] itemReferences;
 
@@ -6,12 +9,25 @@ class PriceQuery {
     }
 
     Result findPrice(String soughtItemCode) {
-        for (ItemReference itemReference:
-             itemReferences) {
-            if (itemReference.matchesSoughtItemCode(soughtItemCode)) {
-                return Result.found(itemReference.getUnitPrice());
-            }
+        return reduce(Result.notFound(soughtItemCode),
+                (result, itemReference) -> {
+                    if (itemReference.matchesSoughtItemCode(soughtItemCode)) {
+                        return Result.found(itemReference.getUnitPrice());
+                    } else {
+                        return result;
+                    }
+                },
+                Arrays.asList(itemReferences));
+    }
+
+    private <R, T> R reduce(R identity,
+                            BiFunction<R, T, R> reducer,
+                            Iterable<T> values) {
+        R accumulator = identity;
+        for (T value :
+                values) {
+            accumulator = reducer.apply(accumulator, value);
         }
-        return Result.notFound(soughtItemCode);
+        return accumulator;
     }
 }
