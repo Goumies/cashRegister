c7f80dd3d90771155960f8c76d4a25566cd2f30f
Romy <contact@romyalula.com>
2019-01-30 16:10:00 +0100

    Cash Register : Search for price based on a Map built with a stream *** findPrice changed to a ternary condition after a get on the unitPriceByItemCode map

--- src/main/java/PriceQuery.java
+++ src/main/java/PriceQuery.java
@@ -1,18 +1,23 @@
+import java.util.Map;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.toMap;
+
 class PriceQuery {
-    private final ItemReference[] itemReferences;
+    private final Map<String, Price> unitPriceByItemCode;
 
     PriceQuery(ItemReference... itemReferences) {
-        this.itemReferences = itemReferences;
+        this.unitPriceByItemCode = Stream.of(itemReferences)
+                .collect(toMap(
+                        ItemReference::getItemCode,
+                        ItemReference::getUnitPrice
+                ));
     }
 
     Result findPrice(String soughtItemCode) {
-        return Stream.of(itemReferences)
-                .filter(itemReference -> itemReference.matchesSoughtItemCode(soughtItemCode))
-                .map(ItemReference::getUnitPrice)
-                .map(Result::found)
-                .findFirst()
-                .orElseGet(() -> Result.notFound(soughtItemCode));
+        Price price = unitPriceByItemCode.get(soughtItemCode);
+        return price != null
+                ? Result.found(price)
+                : Result.notFound(soughtItemCode);
     }
 }
